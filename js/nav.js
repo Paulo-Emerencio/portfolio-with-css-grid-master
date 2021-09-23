cff4ff9addb4e8315c340c365be9ef187847549a
Paulo Emerencio <p.emerencio@gmail.com>
2021-09-23 10:09:40 -0300

    fixing bugs

--- js/nav.js
+++ js/nav.js
@@ -1,5 +1,13 @@
 const navToggle = document.querySelector('.menu-button');
+const nav = document.querySelector('nav');
+const containerAll = document.querySelector('.container-all');
 
 navToggle.addEventListener('click', _ => {
+   containerAll.style.transition = 'transform ease-out 250ms';
    document.body.classList.toggle('nav-is-open');
+});
+
+nav.addEventListener('click', _ => {
+   containerAll.style.transition = 'none'
+   document.body.classList.remove('nav-is-open');
 });
